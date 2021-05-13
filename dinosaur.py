a3a1ec79210ef6facaf2fe674c210bb713685f8a
unknown <schmidt1519@gmail.com>
2021-05-13 12:17:26 -0500

    Created weapon, robot, and dinosaur py files with respective properties for each. Imported to main.py

---
+++ dinosaur.py
@@ -0,0 +1,8 @@
+class Dinosaur:
+    def __init__(self, type, attack_power):
+        self.type = type
+        self.health = 100
+        self.energy = 150
+        self.attack_power = 50
+
+    def
---
+++ robot.py
@@ -0,0 +1,8 @@
+class Robot:
+    def __init__(self, name):
+        self.name = name
+        self.health = 100
+        self.power_level = 50
+        self.weapon = Weapon
+
+    def
